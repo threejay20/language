8ea6061baf9c125bef46406262283f4de0d9fece
threejay20 <118399217+threejay20@users.noreply.github.com>
2024-03-19 19:19:31 +0800

    button started

---
+++ language/app/buttons/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+
+const ButtonsPage = () => {
+  return (
+    <div className="p-4 space-y-4 flex flex-col max-w-[200]">
+      <Button>Default</Button>
+      <Button>Primary</Button>
+      <Button>Primary Outline</Button>
+    </div>
+  );
+};
+
+export default ButtonsPage;
--- language/app/page.tsx
+++ language/app/page.tsx
@@ -1,11 +1,9 @@
-import { Button } from "@/components/ui/button"
-
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
-return (
-  <div>
-   <Button size="lg" > Click Me </Button>
-   
-  </div>
-)
+  return (
+    <div>
+      <Button size="lg"> Click Me </Button>
+    </div>
+  );
 }
