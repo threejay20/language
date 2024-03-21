0f3626ba42e8bb3da61214427cee285ae0e45093
threejay20 <118399217+threejay20@users.noreply.github.com>
2024-03-21 19:00:40 +0800

    button component done

--- language/app/buttons/page.tsx
+++ language/app/buttons/page.tsx
@@ -4,8 +4,17 @@ const ButtonsPage = () => {
   return (
     <div className="p-4 space-y-4 flex flex-col max-w-[200]">
       <Button>Default</Button>
-      <Button>Primary</Button>
-      <Button>Primary Outline</Button>
+      <Button variant="primary">Primary</Button>
+      <Button variant="primaryOutline">Primary Outline</Button>
+      <Button variant="secondary">Secondary</Button>
+      <Button variant="secondaryOutline">Secondary Outline</Button>
+      <Button variant="danger">Danger</Button>
+      <Button variant="dangerOutline">Danger Outline</Button>
+      <Button variant="super">Super</Button>
+      <Button variant="superOutline">Super Outline</Button>
+      <Button variant="ghost">Ghost</Button>
+      <Button variant="sidebar">Side bar</Button>
+      <Button variant="sidebarOutline">Side bar Outline</Button>
     </div>
   );
 };
