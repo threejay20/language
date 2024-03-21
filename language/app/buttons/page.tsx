9a8a10f58c0bb8d95af9111cc7d9bfa274952149
threejay20 <118399217+threejay20@users.noreply.github.com>
2024-03-21 19:05:55 +0800

    fixed formatting

--- language/app/buttons/page.tsx
+++ language/app/buttons/page.tsx
@@ -2,7 +2,7 @@ import { Button } from "@/components/ui/button";
 
 const ButtonsPage = () => {
   return (
-    <div className="p-4 space-y-4 flex flex-col max-w-[200]">
+    <div className="p-4 space-y-4 flex flex-col max-w-[200px]">
       <Button>Default</Button>
       <Button variant="primary">Primary</Button>
       <Button variant="primaryOutline">Primary Outline</Button>
