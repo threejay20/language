ef67040f91ed60da0cb5cc74ca27f806d5cd8d1d
threejay20 <118399217+threejay20@users.noreply.github.com>
2024-04-03 15:41:49 +0800

    user progress issue fixed

--- language/components/user-progress.tsx
+++ language/components/user-progress.tsx
@@ -1,20 +1,22 @@
 import Link from "next/link";
-import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import { InfinityIcon } from "lucide-react";
 
+import { courses } from "@/db/schema";
+import { Button } from "@/components/ui/button";
+
 type Props = {
-  activeCourse: { imageSrc: string; title: string }; //Todo: Replace with DB types
+  activeCourse: typeof courses.$inferSelect;
   hearts: number;
   points: number;
   hasActiveSubscription: boolean;
 };
 
-export const UserProgress = ({
-  activeCourse,
-  points,
-  hearts,
-  hasActiveSubscription,
+export const UserProgress = ({ 
+  activeCourse, 
+  points, 
+  hearts, 
+  hasActiveSubscription
 }: Props) => {
   return (
     <div className="flex items-center justify-between gap-x-2 w-full">
@@ -31,30 +33,17 @@ export const UserProgress = ({
       </Link>
       <Link href="/shop">
         <Button variant="ghost" className="text-orange-500">
-          <Image
-            src="/points.svg"
-            height={28}
-            width={28}
-            alt="Points"
-            className="mr-2"
-          />
+          <Image src="/points.svg" height={28} width={28} alt="Points" className="mr-2" />
           {points}
         </Button>
       </Link>
       <Link href="/shop">
         <Button variant="ghost" className="text-rose-500">
-          <Image
-            src="/heart.svg"
-            height={22}
-            width={22}
-            alt="Hearts"
-            className="mr-2"
-          />
-          {hasActiveSubscription ? (
-            <InfinityIcon className="h-4 w-4 stroke-[3]" />
-          ) : (
-            hearts
-          )}
+          <Image src="/heart.svg" height={22} width={22} alt="Hearts" className="mr-2" />
+          {hasActiveSubscription 
+            ? <InfinityIcon className="h-4 w-4 stroke-[3]" /> 
+            : hearts
+          }
         </Button>
       </Link>
     </div>
