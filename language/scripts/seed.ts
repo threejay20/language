acfc5c7d8dbed60a94eeaa0027dfcb83fa39bcde
threejay20 <118399217+threejay20@users.noreply.github.com>
2024-04-15 16:45:29 +0800

    troubleshooted and fixed units error

--- language/scripts/seed.ts
+++ language/scripts/seed.ts
@@ -16,6 +16,11 @@ const main = async () => {
 
     await db.delete(schema.courses);
     await db.delete(schema.userProgress);
+    await db.delete(schema.units);
+    await db.delete(schema.lessons);
+    await db.delete(schema.challenges);
+    await db.delete(schema.challengeOptions);
+    await db.delete(schema.challengeProgress);
 
     await db.insert(schema.courses).values([
       {
@@ -42,6 +47,61 @@ const main = async () => {
         id: 5,
         title: "Japanese",
         imageSrc: "/jp.svg",
+      },
+    ]);
+
+    await db.insert(schema.units).values([
+      {
+        id: 1,
+        title: "Unit 1",
+        description: "Learn the basics of spanish",
+        courseId: 1,
+        order: 1,
+      },
+    ]);
+
+    await db
+      .insert(schema.lessons)
+      .values([{ id: 1, unitId: 1, order: 1, title: "Nouns" }]);
+
+    await db
+      .insert(schema.lessons)
+      .values([{ id: 2, unitId: 1, order: 2, title: "Verbs" }]);
+
+    await db.insert(schema.challenges).values([
+      {
+        id: 1,
+        lessonId: 1,
+        type: "SELECT",
+        order: 1,
+        question: 'Which one of these is the "the man"',
+      },
+    ]);
+
+    await db.insert(schema.challengeOptions).values([
+      {
+        id: 1,
+        challengeId: 1,
+        imageSrc: "/man.svg",
+        isCorrect: true,
+        text: "El hombre",
+        audioSrc: "/es_man.mp3",
+      },
+      {
+        id: 2,
+        challengeId: 1,
+        imageSrc: "/woman.svg",
+        isCorrect: false,
+        text: "La mujer",
+        audioSrc: "/es_woman.mp3",
+      },
+      {
+        id: 3,
+        challengeId: 1,
+        imageSrc: "/robot.svg",
+        isCorrect: false,
+        text: "el robot",
+        audioSrc: "/es_robot.mp3",
       },
     ]);
 
