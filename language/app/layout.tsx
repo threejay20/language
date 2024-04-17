b7ef1d134463a287e05ca6ad4fbf6ff1d2a15b64
threejay20 <118399217+threejay20@users.noreply.github.com>
2024-04-17 19:53:24 +0800

    exit modal

--- language/app/layout.tsx
+++ language/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Nunito } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
+import { ExitModal } from "@/components/modals/exit-modal";
 
 const font = Nunito({ subsets: ["latin"] });
 
@@ -18,7 +19,10 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={font.className}>{children}</body>
+        <body className={font.className}>
+          <ExitModal />
+          {children}
+        </body>
       </html>
     </ClerkProvider>
   );
