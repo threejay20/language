ff1c41a29cd4a1eb8fd6d9aef2ed703ff27f7815
threejay20 <118399217+threejay20@users.noreply.github.com>
2024-04-17 18:04:05 +0800

    Course progress updated

--- language/app/(main)/learn/unit.tsx
+++ language/app/(main)/learn/unit.tsx
@@ -10,7 +10,7 @@ type Props = {
   lessons: (typeof lessons.$inferSelect & { completed: boolean })[];
   activeLesson:
     | (typeof lessons.$inferSelect & { unit: typeof units.$inferSelect })
-    | null;
+    | undefined;
 
   activeLessonPercentage: number;
 };
